162dc5c4e9b2f8657a21ca9be660a6da4b5171e6
ht <2790372069@qq.com>
2025-04-18 15:47:12 +0800

    chore: 修改项目结构

--- apps/chrome-extension/vite.config.ts
+++ apps/chrome-extension/vite.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import DevPreview from './plugins/dev-preview' // Import the custo
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import DevPreview from './plugins/dev-preview'; // Import the custo
+import tailwindcss from '@tailwindcss/vite';
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,23 +12,25 @@ export default defineConfig({
     sourcemap: false, // Generate source maps for debugging
     rollupOptions: {
       input: {
-        'index': path.resolve(__dirname, 'index.html'), // Specify the entry point for the index// Specify the entry point for the index
-        'worker': path.resolve(__dirname, 'src/scripts/worker.ts'), // Specify the entry point for the worker
-        'content': path.resolve(__dirname, 'src/scripts/content.ts'), // Specify the entry point for the content
+        index: path.resolve(__dirname, 'index.html'), // Specify the entry point for the index// Specify the entry point for the index
+        worker: path.resolve(__dirname, 'src/scripts/worker.ts'), // Specify the entry point for the worker
+        content: path.resolve(__dirname, 'src/scripts/content.ts'), // Specify the entry point for the content
       },
       output: {
         /**index单独成包 */
         entryFileNames: (chunkInfo) => {
-          return chunkInfo.name === 'index' ? 'assets/[name].js' : 'scripts/[name].js';
+          return chunkInfo.name === 'index'
+            ? 'assets/[name].js'
+            : 'scripts/[name].js';
         },
         chunkFileNames: 'assets/[name].js', // Use the chunk name as the output file name
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
+        assetFileNames: 'assets/[name].[ext]',
+      },
+    },
   },
   resolve: {
     alias: {
       '@': path.join(__dirname, './src'),
-    }
-  }
-})
+    },
+  },
+});
